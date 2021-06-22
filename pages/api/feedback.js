5c004e5431c748236724c8f1ae99c96cc2383ed3
Ibwedagama <wardana.wedagama@gmail.com>
2021-06-22 21:06:35 +0700

    create data folder; save data from API to json file

--- pages/api/feedback.js
+++ pages/api/feedback.js
@@ -1,7 +1,52 @@
+import fs from 'fs'
+import path from 'path'
+
 function handler(req, res) {
-  res.status(200).json({
-    message: 'This works'
-  })
+  if (req.method === 'POST') {
+    const email = req.body.email.text
+    const feedback = req.body.text
+
+    const newFeedback = {
+      id: new Date().toISOString(),
+      email,
+      feedback
+    }
+
+    //  store database in a file
+
+    /**
+     * create a file path using path module from Node.js
+     */
+    const filePath = path.join(process.cwd(), 'data', 'feedback.json')
+
+    /**
+     * read a file that lives in `filePath` directory
+     */
+    const fileData = fs.readFileSync(filePath)
+
+    /**
+     * create data 
+     */
+    const data = JSON.parse(fileData)
+    data.push(newFeedback)
+
+    /**
+     * write new data in `filePath` directory from `data` object before
+     */
+    fs.writeFileSync(filePath, JSON.stringify(data))
+
+    /**
+     * send response from API 
+     */
+    res.status(201).json({
+      message: "Success",
+      feedback: newFeedback
+    })
+  } else {
+    res.status(200).json({
+      message: "This Works!"
+    })
+  }
 }
 
 export default handler
