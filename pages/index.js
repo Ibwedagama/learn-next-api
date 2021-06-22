9d28a047f5c819bd4be50a6a7ccc9bfcecdcca53
Ibwedagama <wardana.wedagama@gmail.com>
2021-06-22 20:48:18 +0700

    add form to index page

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,20 @@
 export default function HomePage() {
   return (
     <div>
-     <h1>Home Page</h1>
+      <h1>Home Page</h1>
+      <form action="">
+        <div>
+          <label htmlFor="email">Your email address</label>
+          <input type="email" id="email" />
+        </div>
+        <div>
+          <label htmlFor="feedback">Insert your feedback</label>
+          <textarea type="text" id="feedback" />
+        </div>
+        <div>
+          <button type="submit">Send your feedback!</button>
+        </div>
+      </form>
     </div>
   )
 }
